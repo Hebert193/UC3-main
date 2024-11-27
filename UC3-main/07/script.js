2145be945b300dc2d7eea877294c71f3f0ffcddd
hebert <heberte.2004@gmail.com>
2024-11-26 22:11:57 -0300

    aprendendo js

---
+++ UC3-main/07/script.js
@@ -0,0 +1,30 @@
+
+function saudacao() {
+    console.log("Olá, JavaScript");
+}
+
+function calculaODobro(valorOriginal) {
+    let dobro = valorOriginal * 2;
+    return dobro;
+}
+
+let resposta = calculaODobro(7);
+console.log(resposta);
+
+
+// Faça essa função receber um parametro "nome"
+// Ela deve retonar "Olá, {nome}"
+function saudacao(nome) {
+    console.log("Olá, " + nome);
+}
+
+function calculaODobro(valorOriginal) {    
+    return valorOriginal * 2;
+}
+
+function multiplicar(a, b) {    
+    return a * b;
+}
+
+let resultado = multiplicar(4, 5);
+console.log(resultado);
